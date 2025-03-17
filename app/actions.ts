86680ef479fbb00fff11901415f32fb6f2d4103b
Aryan Saxena <aryansaxena@gmail.com>
2025-03-17 23:31:57 +0530

    added production link

--- app/actions.ts
+++ app/actions.ts
@@ -76,7 +76,8 @@ export async function createInvoice(prevState: any, formData: FormData) {
             amount:data.total,
             currency:data.currency as any
         }),
-        invoiceLink:`http://localhost:3000/api/invoice/${data.id}`
+        invoiceLink: (process.env.NODE_ENV !== "production")? `http://localhost:3000/api/invoice/${data.id}`
+        : `https://invoice-platform-ten.vercel.app/api/invoice/${data.id}`
     })
 
     return redirect("/dashboard/invoices")
@@ -128,7 +129,8 @@ export async function editInvoice(prevState: any, formData: FormData) {
             amount:data.total,
             currency:data.currency as any
         }),
-       invoiceLink:`http://localhost:3000/api/invoice/${data.id}`
+       invoiceLink: (process.env.NODE_ENV !== "production")? `http://localhost:3000/api/invoice/${data.id}`
+        : `https://invoice-platform-ten.vercel.app/api/invoice/${data.id}`
     })
 
     return redirect("/dashboard/invoices")
